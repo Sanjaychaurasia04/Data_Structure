4a2dcff275f275b803e66815fafc375a09c97b87
Sanjay Chaurasia <168840203+Sanjaychaurasia04@users.noreply.github.com>
2025-06-28 21:55:26 +0530

    pascals triangle

---
+++ Array/Pascalstrinagle.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void pascals(int num){
+    vector<vector<int>>ans;
+
+    for(int i =0;i<num;i++){
+        ans.push_back(vector<int>(i+1));
+        for(int j =0;j<i+1;j++){
+            if(j ==0 || j==i){
+                ans[i][j] = 1;
+
+            }
+            else{
+                ans[i][j] = ans[i-1][j]+ ans[i-1][j-1];
+            }
+        }
+    }
+    for(int  i=0;i<ans.size();i++){
+        for(int j =0;j<i+1;j++){
+            cout<<ans[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+int main(){
+    int num =5;
+    pascals(num);
+
+
+    return 0;
+}
